24c4daf527f9517c48b866d0f76916197ccc0163
Adam <adam.lj.haglund@gmail.com>
2020-05-01 18:21:37 +0200

    add stuff

--- api/login.ts
+++ api/login.ts
@@ -1,6 +1,25 @@
-import { NowRequest, NowResponse } from '@now/node'
+import { NowRequest, NowResponse } from "@now/node"
 
-export default (req: NowRequest, res: NowResponse) => {
-  req.
-  res.json({ name: 'John', email: 'john@example.com' })
+import { createSession } from "../src/crunchyroll"
+
+export default async (req: NowRequest, res: NowResponse) => {
+  const { refreshToken } = req.query
+  const result = await createSession(
+    Array.isArray(refreshToken) ? refreshToken[1] : refreshToken
+  )
+
+  res.json({
+    token: result.data.auth,
+    expiresAt: result.data.expires,
+    expiresInMs: new Date(result.data.expires).getTime() - Date.now(),
+    user:
+      result.data.user == null
+        ? null
+        : {
+            id: result.data.user.user_id,
+            username: result.data.user.username,
+            email: result.data.user.email,
+            isPremium: result.data.user.access_type === "premium",
+          },
+  })
 }
